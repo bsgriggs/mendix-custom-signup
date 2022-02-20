bb71597189857c7a80a12969c601699b7f66f7c3
bsgriggs <benjaminsgriggs@gmail.com>
2022-02-20 14:39:00 -0500

    initial commit

---
+++ deployment/native/styles.js
@@ -0,0 +1,12 @@
+import * as style0 from "D:/Projects/Mendix/CustomSignUp/themesource/administration/native/main";
+import * as style1 from "D:/Projects/Mendix/CustomSignUp/themesource/anychart_buildingblocks/native/main";
+import * as style2 from "D:/Projects/Mendix/CustomSignUp/themesource/nanoflowcommons/native/main";
+import * as style3 from "D:/Projects/Mendix/CustomSignUp/themesource/webactions/native/main";
+import * as style4 from "D:/Projects/Mendix/CustomSignUp/themesource/atlas_core/native/main";
+import * as style5 from "D:/Projects/Mendix/CustomSignUp/themesource/myfirstmodule/native/main";
+import * as style6 from "D:/Projects/Mendix/CustomSignUp/themesource/signup/native/main";
+import * as style7 from "D:/Projects/Mendix/CustomSignUp/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7]);
